4166714fe9de0f3ba82ea8bf1bbd845b8b489588
Daniel Ideriba <danielideriba@Daniels-Mac-mini-2.local>
2018-09-05 01:02:07 -0300

    First commit

---
+++ ComprasExterior/ShoppingListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingListViewController.swift
+//  ComprasExterior
+//
+//  Created by Daniel Ideriba on 02/09/18.
+//  Copyright © 2018 Daniel Ideriba. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
